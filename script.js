17957f0fc39543935f70831baf5d97c649b0a15b
yosser balti <yosserbalti49@gmail.com>
2024-12-07 14:35:26 +0100

    first commit

--- script.js
+++ script.js
@@ -3,6 +3,8 @@ function validateForm() {
     const email = document.getElementById('email').value.trim();
     const message = document.getElementById('message').value.trim();
 
+
+
     if (!name || !email || !message) {
         alert("Tous les champs sont obligatoires !");
         return false;
@@ -16,4 +18,77 @@ function validateForm() {
     alert("Message envoyé avec succès !");
     return true;
 }
+
+// Montrer le bouton lorsque l'utilisateur fait défiler la page
+window.onscroll = function() {
+    scrollFunction();
+};
+
+function scrollFunction() {
+    if (document.body.scrollTop > 50 || document.documentElement.scrollTop > 50) {
+        document.getElementById("scrollTopBtn").style.display = "block";
+    } else {
+        document.getElementById("scrollTopBtn").style.display = "none";
+    }
+}
+
+// Fonction pour faire défiler la page vers le haut
+function scrollToTop() {
+    document.body.scrollTop = 0;
+    document.documentElement.scrollTop = 0;
+}
+
+
+
+document.addEventListener('DOMContentLoaded', () => {
+    const ctx = document.getElementById('skillsChart').getContext('2d');
+    
+    const data = {
+        labels: ['HTML', 'CSS', 'JavaScript', 'React', 'Node.js', 'MongoDB', 'Postman'],
+        datasets: [{
+            label: 'Niveau de Compétence (%)',
+            data: [100, 100, 80, 80, 70, 60, 60],
+            backgroundColor: [
+                'rgba(75, 192, 192, 0.2)',
+                'rgba(54, 162, 235, 0.2)',
+                'rgba(255, 206, 86, 0.2)',
+                'rgba(255, 99, 132, 0.2)',
+                'rgba(153, 102, 255, 0.2)',
+                'rgba(255, 159, 64, 0.2)',
+                'rgba(201, 203, 207, 0.2)'
+            ],
+            borderColor: [
+                'rgba(75, 192, 192, 1)',
+                'rgba(54, 162, 235, 1)',
+                'rgba(255, 206, 86, 1)',
+                'rgba(255, 99, 132, 1)',
+                'rgba(153, 102, 255, 1)',
+                'rgba(255, 159, 64, 1)',
+                'rgba(201, 203, 207, 1)'
+            ],
+            borderWidth: 1
+        }]
+    };
+
+    const options = {
+        indexAxis: 'y',
+        responsive: true,
+        plugins: {
+            legend: {
+                display: true,
+                position: 'top'
+            }
+        },
+        scales: {
+            x: {
+                beginAtZero: true,
+                max: 100
+            }
+        }
+    };
+
+
+});
+
+
 
